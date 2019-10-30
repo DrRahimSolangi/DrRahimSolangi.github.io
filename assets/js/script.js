dfea7f4ff7430a5f8c3886627a7603fd55799d15
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-10-30 18:08:55 +0100

    script added for the buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,41 @@
 //     $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
 //   });
 // });
+
+const bio = $("#bio");
+const awards = $("#awards");
+const gallery = $("#gallery");
+const blog = $("#blog");
+
+bio.click(function(e) {
+  awards.removeClass("activeBTN");
+  gallery.removeClass("activeBTN");
+  blog.removeClass("activeBTN");
+  bio.addClass("activeBTN");
+
+  e.preventDefault();
+});
+awards.click(function(e) {
+  bio.removeClass("activeBTN");
+  gallery.removeClass("activeBTN");
+  blog.removeClass("activeBTN");
+  awards.addClass("activeBTN");
+
+  e.preventDefault();
+});
+gallery.click(function(e) {
+  awards.removeClass("activeBTN");
+  bio.removeClass("activeBTN");
+  blog.removeClass("activeBTN");
+  gallery.addClass("activeBTN");
+
+  e.preventDefault();
+});
+blog.click(function(e) {
+  awards.removeClass("activeBTN");
+  gallery.removeClass("activeBTN");
+  bio.removeClass("activeBTN");
+  blog.addClass("activeBTN");
+
+  e.preventDefault();
+});
